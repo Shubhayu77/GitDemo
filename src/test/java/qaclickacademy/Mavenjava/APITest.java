ef574b4991d19cb4da2ec3098b3e3238d8b7d59f
Shubhayu77 <mondalshubhayu77@gmail.com>
2021-05-31 11:13:37 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/APITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test
+	public void nativeMblAutomation()
+	{
+		System.out.println("Native Mbl Automation");
+	}
+	@Test
+	public void iosAutomation()
+	{
+		System.out.println("Ios Automation");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("Browser Automation");
+	}
+	@Test
+	public void uiAutomation()
+	{
+		System.out.println("Ui Automation");
+	}
+}
